db607e3a4fb640a735e6badaa859ea18583edf39
eysaif <shaikh.saifuddin@in.ey.com>
2022-12-19 14:41:40 +0530

    html and css changes

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ app.get("/chat",(req,res)=>{
 const io = require("socket.io")(server)
 
 io.on('connection',(socket)=>{
-    console.log("---ClientConnected----")
+    console.log("---ClientConnected----",socket.id)
 })
--- public/clientApp.js
+++ public/clientApp.js
@@ -1,5 +1,12 @@
 const socket = io()
 
+let userName;
+
+do{
+    userName = prompt("Enter Your name: ");
+}while(!userName)
+
+
 function sendMsg(){
-    console.warn("---msg Send");
+    console.warn("---msg Send",userName,socket.id);
 }
